af12d718cc3d818bcdccc2f94a2603f12a71898f
Hidayat arghandabi <hidayatarg@gmail.com>
2019-04-12 14:50:22 +0300

    Serving Html Page

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const path = require('path')
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello from nodejs</h1>')
+    // go one level up
+    res.sendFile(path.join(__dirname,'../','views','shop.html'));
 });
 
 

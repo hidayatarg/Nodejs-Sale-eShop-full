28abdd4e51148238fa8e39e6aac45752474756e2
Hidayat arghandabi <hidayatarg@gmail.com>
2019-04-19 17:11:29 +0300

    Added the Active class to Add-Product [Passing a Class]

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,7 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.status(200).render('add-product', { pageTitle: 'Add Product'});
+    res.status(200).render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
     
 // /admin/add-product => POST

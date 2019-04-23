021ed91a3003d54b7e787d51f4fc6eb3edebb89d
Hidayat Arghandabi <hidayatarg@gmail.com>
2019-04-23 13:51:50 +0300

    Storing the Data in File

--- models/product.js
+++ models/product.js
@@ -1,19 +1,54 @@
 // an Entity
 
-const products = []
+// path
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../utils/path')
+
 module.exports = class Product {
     constructor(t){
         this.title = t
     }
 
     // function 
-    // this refer to the object that will be stored
     save() {
-        products.push(this);
+        // root project folder data folder and store file
+        const p = path.join(
+            rootDir, 
+            'data', 
+            'products.json'
+        );
+        // read existing products
+        fs.readFile(p, (err, fileData) => {
+            let products = [];
+            console.log(fileData);
+            if (!err) {
+                // convert JSON to array
+                products = JSON.parse(fileData);
+            }
+            // this refere to the class
+            products.push(this)
+            // Stringfiy conver to JSON
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);                
+            });
+            
+        });
     }
 
     static fetchAll() {
-        return products;
+        const p = path.join(
+            rootDir,
+            'data',
+            'products.json'
+        );
+        fs.readFile(p, (err, fileData) =>{
+            if (err) {
+                // no product
+                return [];
+            }
+            return JSON.parse(fileData);
+        });
     }
 
 }

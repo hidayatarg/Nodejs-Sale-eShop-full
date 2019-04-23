534d79b12bf588caba7e78a6fd48864767159263
Hidayat Arghandabi <hidayatarg@gmail.com>
2019-04-23 14:08:20 +0300

    Completed the Product saving to the  File

--- models/product.js
+++ models/product.js
@@ -5,6 +5,24 @@ const fs = require('fs');
 const path = require('path');
 const rootDir = require('../utils/path')
 
+const p = path.join(
+    rootDir,
+    'data',
+    'products.json'
+);
+
+// Helper function
+const getProductFromFile = (cb) => {
+    fs.readFile(p, (err, fileData) => {
+        if (err) {
+            // no product
+            cb([]);
+        } else {
+            cb(JSON.parse(fileData));
+        }
+    });
+};
+
 module.exports = class Product {
     constructor(t){
         this.title = t
@@ -12,43 +30,17 @@ module.exports = class Product {
 
     // function 
     save() {
-        // root project folder data folder and store file
-        const p = path.join(
-            rootDir, 
-            'data', 
-            'products.json'
-        );
-        // read existing products
-        fs.readFile(p, (err, fileData) => {
-            let products = [];
-            console.log(fileData);
-            if (!err) {
-                // convert JSON to array
-                products = JSON.parse(fileData);
-            }
-            // this refere to the class
-            products.push(this)
-            // Stringfiy conver to JSON
-            fs.writeFile(p, JSON.stringify(products), (err) => {
+        // recieve a products
+        getProductFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => {
                 console.log(err);                
-            });
-            
+            })
         });
     }
 
     static fetchAll(cb) {
-        const p = path.join(
-            rootDir,
-            'data',
-            'products.json'
-        );
-        fs.readFile(p, (err, fileData) =>{
-            if (err) {
-                // no product
-                cb([]);
-            }
-            cb(JSON.parse(fileData));
-        });
+        getProductFromFile(cb);
     }
 
 }

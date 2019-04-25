0da24af1c06e5da9925aaa0529183c8ee60c8a87
Hidayat Arghandabi <hidayatarg@gmail.com>
2019-04-25 14:40:14 +0300

    Added the Orders Pages, Route and In Shop Controller

--- routes/shop.js
+++ routes/shop.js
@@ -13,5 +13,6 @@ router.get('/products', shopController.getProducts );
 router.get('/cart', shopController.getCart);
 
 router.get('/checkout', shopController.getCheckout);
+router.get('/orders', shopController.getOrders);
 
 module.exports = router;
